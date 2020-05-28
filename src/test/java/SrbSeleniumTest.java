fd99de493301ee5298ac041eb9899d39bf4ec29b
Kharitonov Konstantin <karlyuhenson@gmail.com>
2020-05-29 00:03:13 +0300

    two commit

--- src/test/java/SrbSeleniumTest.java
+++ src/test/java/SrbSeleniumTest.java
@@ -1,56 +1,9 @@
 import org.junit.Assert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
-import java.time.Duration;
-import java.util.Arrays;
-import java.util.Collection;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsEqual.equalTo;
-@RunWith( Parameterized.class )
 public class SrbSeleniumTest extends StartUpTest {
-    @Parameterized.Parameters
-    public static Collection <Object[]> param ( ) {
-
-        Object[] one = {"Ivanov" , "Ivan" , "23011988" , "Иванов" , "Иван" , "Иванович" , "24011989" , "1111" , "24021989" , "24012018" , "УВД Москвы"};
-        Object[] two = {"Petrov" , "Petr" , "23041987" , "Петров" , "Петр" , "Петрович" , "25081986" , "2222" , "222222" , "24012019" , "УВД Волгоград"};
-        Object[] three = {"Sergeev" , "Sergey" , "13121987" , "Сергеев" , "Сергей" , "Сергеевич" , "10101986" , "3333" , "333333" , "10102000" , "Кировский УВД"};
-        return Arrays.asList ( one, two, three);
-
-    }
-
-    @Parameterized.Parameter(0)
-    public String lastNameInsured;
-    @Parameterized.Parameter(1)
-    public  String firstNameInsured;
-    @Parameterized.Parameter(2)
-    public  String birthdayInsured;
-    @Parameterized.Parameter(3)
-    public  String lastNamePolicyholder;
-    @Parameterized.Parameter(4)
-    public  String firstNamePolicyholder;
-    @Parameterized.Parameter(5)
-    public  String middleNamePolicyholder;
-    @Parameterized.Parameter(6)
-    public  String birthdayPolicyholder;
-    @Parameterized.Parameter(7)
-    public  String passportSeries;
-    @Parameterized.Parameter(8)
-    public  String passportNumber;
-    @Parameterized.Parameter(9)
-    public  String passportDate;
-    @Parameterized.Parameter(10)
-    public  String passportIssuedBy;
-    @Parameterized.Parameter(11)
-    public  String continueButton;
-
-
 
 
     @Test
@@ -61,63 +14,77 @@ public class SrbSeleniumTest extends StartUpTest {
         waitElementToBeClickable ( menuLinkXpath );
         //Переход во вкладку "Страхование"
         findElementAndClick ( menuLinkXpath );
-        //Переход на страницу "Страхование путешественников"
         waitElementToBeClickable ( insuranceTravelLinkXpath );
+        //Переход на страницу "Страхование путешественников"
         findElementAndClick ( insuranceTravelLinkXpath );
-
-//        //Проверка наличия баннера с текстом "Страхование путешественников" (падает)
-//        waitPresenceOfElementLocated ("//h1[text()='Страхование путешественников']");
-//        Assert.assertEquals ( "Содержимое ссылки не соответствует ожиданию", "Страхование путешественников",
-//                driver.findElement ( By.xpath ( "//h1[text()='Страхование путешественников']" ) ).getText () );
-
-
-        //Нажатие кнопки "Оформить онлайн"
         waitElementToBeClickable ( takeOutOnlineTravelInsuranceButtonXpath );
+        //Нажатие кнопки "Оформить онлайн"
         findElementAndClick ( takeOutOnlineTravelInsuranceButtonXpath );
-        //Выбор минимальной суммы страховой защиты
         waitElementToBeClickable ( minSummInsuranceXpath );
+        //Выбор минимальной суммы страховой защиты
         findElementAndClick ( minSummInsuranceXpath );
-        //Нажатие кнопки "Оформить"
         waitElementToBeClickable ( sendButtonXpath );
+        //Нажатие кнопки "Оформить"
         findElementAndClick ( sendButtonXpath );
-        //Заполнение поля "Surname" в разделе "Застрахованные"
         waitVisibilityOfElementLocated (lastNameInsuredLatynXpath);
+        //Заполнение поля "Surname" в разделе "Застрахованные"
         findElementAndClick ( lastNameInsuredLatynXpath );
-        sendKeyText ( lastNameInsuredLatynXpath, lastNameInsured );
+        sendKeyText ( lastNameInsuredLatynXpath, "Ivan" );
         //Заполнение поля "Name" в разделе "Застрахованные"
         findElementAndClick(firstNameInsuredLatynXpath);
-        sendKeyText ( firstNameInsuredLatynXpath, firstNameInsured );
+        sendKeyText ( firstNameInsuredLatynXpath, "Ivanov" );
         //Заполнение поля "Дата рождения" в разделе "Застрахованные"
         findElementAndClick(birthdayInsuredXpath);
-        sendKeyText ( birthdayInsuredXpath, birthdayInsured );
+        sendKeyText ( birthdayInsuredXpath, "23.01.1988" );
         //Заполнение поля "Фамилия" в разделе "Страхователь"
         findElementAndClick(lastNamePolicyholderXpath);
-        sendKeyText ( lastNamePolicyholderXpath, lastNamePolicyholder);
+        sendKeyText ( lastNamePolicyholderXpath, "Иванов");
         //Заполнение поля "Имя" в разделе "Страхователь"
         findElementAndClick(firstNamePolicyholderXpath);
-        sendKeyText ( firstNamePolicyholderXpath, firstNamePolicyholder );
+        sendKeyText ( firstNamePolicyholderXpath, "Иван" );
         //Заполнение поля "Отчество" в разделе "Страхователь"
         findElementAndClick ( middleNamePolicyholderXpath );
-        sendKeyText ( middleNamePolicyholderXpath, middleNamePolicyholder );
+        sendKeyText ( middleNamePolicyholderXpath, "Иванович" );
         //Заполнение поля "Отчество" в разделе "Дата рождения"
         findElementAndClick ( birthdayPolicyholderXpath);
-        sendKeyText ( birthdayPolicyholderXpath, birthdayPolicyholder );
+        sendKeyText ( birthdayPolicyholderXpath, "23.01.1988" );
         //Заполнение поля "Серия" в разделе "Паспортные данные"
         findElementAndClick (passportSeriesXpath);
-        sendKeyText (passportSeriesXpath, passportSeries);
+        sendKeyText (passportSeriesXpath, "1111");
         //Заполнение поля "Номер" в разделе "Паспортные данные"
         findElementAndClick (passportNumberXpath);
-        sendKeyText (passportNumberXpath, passportNumber);
+        sendKeyText (passportNumberXpath, "111111");
         //Заполнение поля "Дата выдачи" в разделе "Паспортные данные"
         findElementAndClick (passportDateXpath);
-        sendKeyText (passportDateXpath, passportDate);
+        sendKeyText (passportDateXpath, "08.08.2014");
         //Заполнение поля "Кем выдан" в разделе "Паспортные данные"
         findElementAndClick (passportIssuedByXpath);
-        sendKeyText (passportIssuedByXpath, passportIssuedBy);
+        sendKeyText (passportIssuedByXpath, "ОВД города Москвы");
 
-//        //Проверка заполнения всех полей
-//        Assert.assertEquals ( "Заполните текстовое поле", "Иванов",
-//                driver.findElement ( By.xpath ( "//input[contains(@data-bind, 'value:LastName')]") ).getAttribute("value"));
+        //Проверка заполнения полей
+        Assert.assertEquals ( "Содержимое ссылки не соответствует ожиданию", "Ivan",
+                driver.findElement ( By.xpath (lastNameInsuredLatynXpath) ).getAttribute("value"));
+        Assert.assertEquals ( "Содержимое ссылки не соответствует ожиданию", "Ivanov",
+                driver.findElement ( By.xpath (firstNameInsuredLatynXpath) ).getAttribute("value"));
+        Assert.assertEquals ( "Содержимое ссылки не соответствует ожиданию", "23.01.1988",
+                driver.findElement ( By.xpath (birthdayInsuredXpath) ).getAttribute("value"));
+        Assert.assertEquals ( "Содержимое ссылки не соответствует ожиданию", "Иванов",
+                driver.findElement ( By.xpath (lastNamePolicyholderXpath) ).getAttribute("value"));
+        Assert.assertEquals ( "Содержимое ссылки не соответствует ожиданию", "Иван",
+                driver.findElement ( By.xpath (firstNamePolicyholderXpath) ).getAttribute("value"));
+        Assert.assertEquals ( "Содержимое ссылки не соответствует ожиданию", "Иванович",
+                driver.findElement ( By.xpath (middleNamePolicyholderXpath) ).getAttribute("value"));
+        Assert.assertEquals ( "Содержимое ссылки не соответствует ожиданию", "23.01.1988",
+                driver.findElement ( By.xpath (birthdayPolicyholderXpath) ).getAttribute("value"));
+        Assert.assertEquals ( "Содержимое ссылки не соответствует ожиданию", "1111",
+                driver.findElement ( By.xpath (passportSeriesXpath) ).getAttribute("value"));
+        Assert.assertEquals ( "Содержимое ссылки не соответствует ожиданию", "111111",
+                driver.findElement ( By.xpath (passportNumberXpath) ).getAttribute("value"));
+        Assert.assertEquals ( "Содержимое ссылки не соответствует ожиданию", "08.08.2014",
+                driver.findElement ( By.xpath (passportDateXpath) ).getAttribute("value"));
+        Assert.assertEquals ( "Содержимое ссылки не соответствует ожиданию", "ОВД города Москвы",
+                driver.findElement ( By.xpath (passportIssuedByXpath) ).getAttribute("value"));
+
 
         //Нажатие кнопки "Продолжить"
         waitPresenceOfElementLocated ( continueButtonXpath );
